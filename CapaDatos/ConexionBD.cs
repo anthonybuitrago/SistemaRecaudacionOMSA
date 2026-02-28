1fd1195b0fbe6c9d021278e6571e0a6f729b4431
Anthony <anthonybuitrago8@gmail.com>
2026-02-27 21:48:29 -0400

    DAL: Implementación de la clase de conexión a SQL Server

---
+++ CapaDatos/ConexionBD.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class ConexionBD
+    {
+        private readonly string cadenaConexion = "Server=.;Database=OMSA_Recaudacion;Integrated Security=True;";
+
+        private SqlConnection conexion;
+
+        public ConexionBD()
+        {
+            conexion = new SqlConnection(cadenaConexion);
+        }
+
+        public SqlConnection AbrirConexion()
+        {
+            if (conexion.State == ConnectionState.Closed)
+            {
+                conexion.Open();
+            }
+            return conexion;
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            if (conexion.State == ConnectionState.Open)
+            {
+                conexion.Close();
+            }
+            return conexion;
+        }
+    }
+}

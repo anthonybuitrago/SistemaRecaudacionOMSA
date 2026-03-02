83362e006bcf679a3e82ff520e992db43b9da094
Anthony <anthonybuitrago8@gmail.com>
2026-03-02 14:05:15 -0400

    BLL: Implementación de POO con clase abstracta Persona y herencia en Chofer

---
+++ CapaNegocios/Chofer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CapaNegocios
+{
+    public class Chofer : Persona
+    {
+        public int ID_Chofer { get; set; }
+        public string NumeroLicencia { get; set; }
+        public Chofer(int idChofer, string cedula, string nombreCompleto, string numeroLicencia)
+            : base(cedula, nombreCompleto)
+        {
+            ID_Chofer = idChofer;
+            NumeroLicencia = numeroLicencia;
+        }
+    }
+}
---
+++ CapaNegocios/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CapaNegocios
+{
+
+    public abstract class Persona
+    {
+        public string Cedula { get; set; }
+        public string NombreCompleto { get; set; }
+        public Persona(string cedula, string nombreCompleto)
+        {
+            Cedula = cedula;
+            NombreCompleto = nombreCompleto;
+        }
+    }
+}

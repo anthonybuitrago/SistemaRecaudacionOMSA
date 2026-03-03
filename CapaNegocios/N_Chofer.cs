a883decee7a21020ca81182c8f3163fc8ebc1bb9
Anthony <anthonybuitrago8@gmail.com>
2026-03-03 12:43:52 -0400

    BLL: Creación de clase N_Chofer para intermediar operaciones

---
+++ CapaNegocios/N_Chofer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+using CapaDatos;
+
+namespace CapaNegocios
+{
+    public class N_Chofer
+    {
+        private D_Chofer objDatos = new D_Chofer();
+        public DataTable MostrarChoferes()
+        {
+            return objDatos.Mostrar();
+        }
+        public void InsertarChofer(string cedula, string nombreCompleto, string numeroLicencia)
+        {
+            objDatos.Insertar(cedula, nombreCompleto, numeroLicencia);
+        }
+    }
+}

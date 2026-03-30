07ad95cec7194cb054e798db8e16d2727b95d413
Baez73 <eb2024-3416@uce.edo.do>
2026-03-30 13:04:43 -0400

    diseño

--- SistemaRecaudacionOMSA/FrmLogin.Designer.cs
+++ SistemaRecaudacionOMSA/FrmLogin.Designer.cs
@@ -25,121 +25,124 @@
         private void InitializeComponent()
         {
             this.pnlHeader = new System.Windows.Forms.Panel();
-            this.lblTitulo = new System.Windows.Forms.Label();
             this.lblSubtitulo = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
             this.lblUsuario = new System.Windows.Forms.Label();
             this.lblClave = new System.Windows.Forms.Label();
             this.txtUsuario = new System.Windows.Forms.TextBox();
             this.txtClave = new System.Windows.Forms.TextBox();
             this.btnIngresar = new System.Windows.Forms.Button();
             this.lblError = new System.Windows.Forms.Label();
-
             this.pnlHeader.SuspendLayout();
             this.SuspendLayout();
-
-            // =============================================
-            // PANEL HEADER
-            // =============================================
-            this.pnlHeader.BackColor = System.Drawing.Color.FromArgb(28, 58, 92);
-            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlHeader.Size = new System.Drawing.Size(364, 110);
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
             this.pnlHeader.Controls.Add(this.lblSubtitulo);
             this.pnlHeader.Controls.Add(this.lblTitulo);
-
-            // =============================================
-            // LABEL TITULO
-            // =============================================
-            this.lblTitulo.Text = "Sistema OMSA";
-            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
-            this.lblTitulo.ForeColor = System.Drawing.Color.White;
-            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.lblTitulo.Dock = System.Windows.Forms.DockStyle.Top;
-            this.lblTitulo.Size = new System.Drawing.Size(364, 70);
-
-            // =============================================
-            // LABEL SUBTITULO
-            // =============================================
-            this.lblSubtitulo.Text = "Recaudación";
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(364, 110);
+            this.pnlHeader.TabIndex = 0;
+            // 
+            // lblSubtitulo
+            // 
+            this.lblSubtitulo.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.lblSubtitulo.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.lblSubtitulo.ForeColor = System.Drawing.Color.Silver;
-            this.lblSubtitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.lblSubtitulo.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblSubtitulo.Location = new System.Drawing.Point(0, 80);
+            this.lblSubtitulo.Name = "lblSubtitulo";
             this.lblSubtitulo.Size = new System.Drawing.Size(364, 30);
-
-            // =============================================
-            // LABEL USUARIO
-            // =============================================
-            this.lblUsuario.Text = "Usuario";
+            this.lblSubtitulo.TabIndex = 0;
+            this.lblSubtitulo.Text = "Recaudación";
+            this.lblSubtitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.lblTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.lblTitulo.ForeColor = System.Drawing.Color.White;
+            this.lblTitulo.Location = new System.Drawing.Point(0, 0);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(364, 70);
+            this.lblTitulo.TabIndex = 1;
+            this.lblTitulo.Text = "Sistema OMSA";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblUsuario
+            // 
             this.lblUsuario.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.lblUsuario.ForeColor = System.Drawing.Color.FromArgb(80, 80, 80);
+            this.lblUsuario.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
             this.lblUsuario.Location = new System.Drawing.Point(40, 130);
+            this.lblUsuario.Name = "lblUsuario";
             this.lblUsuario.Size = new System.Drawing.Size(300, 20);
-
-            // =============================================
-            // TEXTBOX USUARIO
-            // =============================================
+            this.lblUsuario.TabIndex = 1;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // lblClave
+            // 
+            this.lblClave.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.lblClave.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.lblClave.Location = new System.Drawing.Point(40, 200);
+            this.lblClave.Name = "lblClave";
+            this.lblClave.Size = new System.Drawing.Size(300, 20);
+            this.lblClave.TabIndex = 3;
+            this.lblClave.Text = "Contraseña";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtUsuario.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.txtUsuario.Location = new System.Drawing.Point(40, 152);
-            this.txtUsuario.Size = new System.Drawing.Size(300, 25);
-            this.txtUsuario.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-
-            // =============================================
-            // LABEL CLAVE
-            // =============================================
-            this.lblClave.Text = "Contraseña";
-            this.lblClave.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.lblClave.ForeColor = System.Drawing.Color.FromArgb(80, 80, 80);
-            this.lblClave.Location = new System.Drawing.Point(40, 200);
-            this.lblClave.Size = new System.Drawing.Size(300, 20);
-
-            // =============================================
-            // TEXTBOX CLAVE (Condición 13 - contraseña oculta)
-            // =============================================
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(300, 34);
+            this.txtUsuario.TabIndex = 2;
+            // 
+            // txtClave
+            // 
+            this.txtClave.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtClave.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.txtClave.Location = new System.Drawing.Point(40, 222);
-            this.txtClave.Size = new System.Drawing.Size(300, 25);
-            this.txtClave.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtClave.Name = "txtClave";
+            this.txtClave.Size = new System.Drawing.Size(300, 34);
+            this.txtClave.TabIndex = 4;
             this.txtClave.UseSystemPasswordChar = true;
             this.txtClave.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtClave_KeyPress);
-
-            // =============================================
-            // BOTON INGRESAR
-            // =============================================
-            this.btnIngresar.Text = "Ingresar";
-            this.btnIngresar.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
-            this.btnIngresar.Location = new System.Drawing.Point(40, 275);
-            this.btnIngresar.Size = new System.Drawing.Size(300, 38);
-            this.btnIngresar.BackColor = System.Drawing.Color.FromArgb(28, 58, 92);
-            this.btnIngresar.ForeColor = System.Drawing.Color.White;
-            this.btnIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnIngresar.FlatAppearance.BorderSize = 0;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.BackColor = System.Drawing.Color.Green;
             this.btnIngresar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIngresar.FlatAppearance.BorderSize = 0;
+            this.btnIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIngresar.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnIngresar.ForeColor = System.Drawing.Color.White;
+            this.btnIngresar.Location = new System.Drawing.Point(40, 275);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(300, 38);
+            this.btnIngresar.TabIndex = 5;
+            this.btnIngresar.Text = "Ingresar";
             this.btnIngresar.UseVisualStyleBackColor = false;
             this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
-
-            // =============================================
-            // LABEL ERROR
-            // =============================================
+            // 
+            // lblError
+            // 
             this.lblError.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.lblError.ForeColor = System.Drawing.Color.Red;
             this.lblError.Location = new System.Drawing.Point(40, 325);
+            this.lblError.Name = "lblError";
             this.lblError.Size = new System.Drawing.Size(300, 20);
+            this.lblError.TabIndex = 6;
             this.lblError.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.lblError.Visible = false;
-
-            // =============================================
-            // FRLOGIN (formulario)
-            // =============================================
-            this.Text = "Sistema OMSA - Recaudación";
+            // 
+            // FrmLogin
+            // 
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(364, 381);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Load += new System.EventHandler(this.FrmLogin_Load);
-
-            // Agregar controles al formulario
             this.Controls.Add(this.pnlHeader);
             this.Controls.Add(this.lblUsuario);
             this.Controls.Add(this.txtUsuario);
@@ -147,10 +150,17 @@
             this.Controls.Add(this.txtClave);
             this.Controls.Add(this.btnIngresar);
             this.Controls.Add(this.lblError);
-
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sistema OMSA - Recaudación";
+            this.Load += new System.EventHandler(this.FrmLogin_Load);
             this.pnlHeader.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
     }
 }

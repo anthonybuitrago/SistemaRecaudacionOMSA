fab029f9bbf2acfa009f89af48762edb7df766c4
Baez73 <eb2024-3416@uce.edo.do>
2026-03-31 19:11:45 -0400

    probando

--- CapaDatos/ConexionBD.cs
+++ CapaDatos/ConexionBD.cs
@@ -5,6 +5,7 @@ using System.Configuration;
 
 namespace CapaDatos
 {
+    // Probando la conexión a la base de datos SQL Server utilizando ADO.NET
     public class ConexionBD
     {
         // Ruta de acceso extraída de las configuraciones del sistema (App.config)
